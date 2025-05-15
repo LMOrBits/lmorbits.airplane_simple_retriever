59ca8f4a8a1c097b5c2f5b51b35d2ee998708242
parsa <parsa.miraghaie@gmail.com>
2025-05-15 14:19:48 +0300

    improved the graph idea

--- src/airplane_simple_retriever/graph.py
+++ src/airplane_simple_retriever/graph.py
@@ -15,29 +15,39 @@ agent = get_agent(config.retriver.search_kwargs)
 def retriver(state: State) -> State:
     result = agent(state.messages)
     return {"retriver_result": result}
+class Graph:
+    name = "airplane-simple-retriever"
+    nodes = {}
+    state = State
 
-def add_workflow(graph: StateGraph , end_node: Optional[str] = None, start_node: Optional[str] = None ):
-    app_name = "airplane-simple-retriever"
-    retriver_name = f"{app_name}.retriver"
-    if start_node and end_node:
-        graph.add_node(retriver_name, retriver)
-        graph.add_edge(start_node, retriver_name)
-        graph.add_edge(retriver_name, end_node)
-    elif start_node:
-        graph.add_node(retriver_name, retriver)
-        graph.add_edge(start_node, retriver_name)
-    elif end_node:
-        graph.add_node(retriver_name, retriver)
-        graph.add_edge(retriver_name, end_node)
-        graph.set_entry_point(retriver_name)
-    else:
-        graph.add_node(retriver_name, retriver)
-        graph.set_entry_point(retriver_name)
-    return graph
+    def __init__(self):
+        retriver_name = f"{self.name}.retriver"
+        self.nodes = {
+            "start": retriver_name,
+            "end": retriver_name
+        }
+    
+    def add_workflow(self,graph: StateGraph , end_node: Optional[str] = None, start_node: Optional[str] = None ):
+        graph.add_node(self.nodes["start"], retriver)
+
+        if start_node and end_node:
+            graph.add_edge(start_node, self.nodes["start"])
+            graph.add_edge(self.nodes["start"], end_node)
+        elif start_node:
+            graph.add_edge(start_node, self.nodes["start"])
+        elif end_node:
+            graph.add_edge(self.nodes["start"], end_node)
+            graph.set_entry_point(self.nodes["start"])
+        else:
+            graph.set_entry_point(self.nodes["start"])
+        return graph
+
+
 
 def get_app():
+    airplane_simple_retriever_graph = Graph()
     graph = StateGraph(State)
-    add_workflow(graph)
+    graph = airplane_simple_retriever_graph.add_workflow(graph)
     return graph.compile()
 
 
